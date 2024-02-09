babbfd81119bd76ff763e00c3fe1d312e476c1b2
uqglmn <48247718+uqglmn@users.noreply.github.com>
2024-02-09 17:46:24 +0000

    Create diagnostics.R

---
+++ slr/diagnostics.R
@@ -0,0 +1,43 @@
+# regression diagnotics plot 
+
+regression_diagnostics <- function(x,y) {
+
+    # set the display size
+    options(repr.plot.width=12, repr.plot.height=6)
+    par(mfrow = c(2,4), mar=c(4,4,3,2))
+
+    model = lm(y~x)           # fit a straight-line model
+    haty  = fitted(model)     # get fitted values, hat-y
+    r     = rstandard(model)  # standardised residuals
+
+    # first row plots:
+    
+    # Regression model plot - 
+    plot(y~x, main="Straight-line model", font.main=1, cex.main=1.5) # adjust the main title
+    abline(model, col=2)                                             # add a fitted regression line
+
+    # Constance of variance: r vs x
+    plot(r~x, ylab="Std residuals", 
+         main="Std residuals vs x", font.main=1, cex.main=1.5)
+    abline(h=0, lty=3, col=8)
+    # the next two lines add a trend-line to the plot - loess fit - locally estimated scatterplot smoothing
+    new.x = seq(min(x), max(x), length.out=20)
+    lines(new.x, predict(loess(r~x), newdata=data.frame(x=new.x)), col=2) # col=2 - red colour
+
+    # Constance of variance: r vs hat-y
+    plot(r~haty, xlab="Fitted values", ylab="Std residuals",
+         main="Std residuals vs Fitted", font.main=1, cex.main=1.5)
+    abline(h=0, lty=3, col=8)
+    # the next two lines add a trend-line to the plot - loess fit - locally estimated scatterplot smoothing
+    new.haty = seq(min(haty),max(haty),length.out=20)
+    lines(new.haty, predict(loess(r~haty), newdata=data.frame(haty=new.haty)), col=2)
+
+    # histogram of r
+    hist(r, main="Std residuals", font.main=1, cex.main=1.5)
+    
+    # second row plots:
+
+    # built-in statistical plots
+    plot(model)
+    
+}
